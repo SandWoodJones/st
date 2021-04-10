7bf6769184f04c7f7d43b90fc9dea21cc5754ba8
SandWood Jones <sandwoodjones@gmail.com>
2021-04-09 23:42:02 -0400

    Applied https://st.suckless.org/patches/alpha_focus_highlight/ now opacity changes depending on wheter terminal is in focus or not

--- config.def.h
+++ config.def.h
@@ -99,7 +99,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 4;
 
 /* bg opacity */
-float alpha = 0.95;
+float alpha = 0.95, alphaUnfocused = 0.75;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
@@ -141,6 +141,7 @@ unsigned int defaultfg = 256;
 unsigned int defaultbg = 257;
 static unsigned int defaultcs = 258;
 static unsigned int defaultrcs = 259;
+unsigned int bg = 257, bgUnfocused = 257;
 
 /*
  * https://invisible-island.net/xterm/ctlseqs/ctlseqs.html#h4-Functions-using-CSI-_-ordered-by-the-final-character-paren-s-rparen:CSI-Ps-SP-q.1D81
