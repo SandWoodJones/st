1ec94edbc557769bfe9d5260f8770f48624b748c
SandWood Jones <sandwoodjones@gmail.com>
2021-04-10 01:25:50 -0400

    Changed the colortheme a bit

--- config.def.h
+++ config.def.h
@@ -139,7 +139,8 @@ static const char *colorname[] = {
 
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#FFFFFF", // default foreground
-	"#000000", // default background
+	"#000503", // default focused background
+	"#000003", // default unfocused background
 	"#FFFFFF", // default cursor
 	"#000000"  // inverse cursor
 };
@@ -151,9 +152,9 @@ static const char *colorname[] = {
  */
 unsigned int defaultfg = 256;
 unsigned int defaultbg = 257;
-static unsigned int defaultcs = 258;
-static unsigned int defaultrcs = 259;
-unsigned int bg = 257, bgUnfocused = 257;
+unsigned int bg = 257, bgUnfocused = 258;
+static unsigned int defaultcs = 259;
+static unsigned int defaultrcs = 260;
 
 /*
  * https://invisible-island.net/xterm/ctlseqs/ctlseqs.html#h4-Functions-using-CSI-_-ordered-by-the-final-character-paren-s-rparen:CSI-Ps-SP-q.1D81
